c5a7949228dec498a98d45b2e41cdfa85e3d97cc
Nnooaahhh <142706005+Nnooaahhh@users.noreply.github.com>
2023-09-02 17:13:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,32 +10,39 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FONT = pygame.font.Font(None, 36)
 
-# Game variables
-balance = 10000
-stock_price = 100
-stock_name = "AAPL"
-stocks_owned = 0
+# Player data
+player_balance = 10000
+avatars = [
+    pygame.image.load("https://www.nicepng.com/png/detail/348-3483446_2d-character-design-wassermolekl.png"),  # Replace with your image file paths
+    pygame.image.load("https://opengameart.org/sites/default/files/player_19.png"),
+    pygame.image.load("https://www.pngitem.com/pimgs/m/190-1906083_2d-girl-character-png-transparent-png.png"),
+]
+avatar_costs = [100, 200, 300]
+selected_avatar = 0
 
-# Function to update stock price
-def update_stock_price():
-    global stock_price
-    stock_price += random.uniform(-5, 5)
+# Stock data (same as before)
+stocks = [
+    {"name": "AAPL", "price": 150.0, "quantity": 0},
+    {"name": "GOOGL", "price": 2700.0, "quantity": 0},
+    # Add more stocks, penny stocks, commodities, etc.
+]
 
-# Function to buy stocks
-def buy_stock():
-    global balance, stocks_owned
-    if balance >= stock_price:
-        stocks_to_buy = min(balance // stock_price, 10)  # Buy at most 10 stocks at a time
-        balance -= stock_price * stocks_to_buy
-        stocks_owned += stocks_to_buy
+# Function to update stock prices (same as before)
+# ...
 
-# Function to sell stocks
-def sell_stock():
-    global balance, stocks_owned
-    if stocks_owned > 0:
-        stocks_to_sell = min(stocks_owned, 10)  # Sell at most 10 stocks at a time
-        balance += stock_price * stocks_to_sell
-        stocks_owned -= stocks_to_sell
+# Function to buy stocks (same as before)
+# ...
+
+# Function to sell stocks (same as before)
+# ...
+
+# Function to purchase an avatar
+def purchase_avatar(avatar_index):
+    global player_balance, selected_avatar
+    cost = avatar_costs[avatar_index]
+    if player_balance >= cost:
+        selected_avatar = avatar_index
+        player_balance -= cost
 
 # Initialize Pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -50,24 +57,38 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 running = False
+            elif event.key == pygame.K_LEFT:
+                selected_avatar = (selected_avatar - 1) % len(avatars)
+            elif event.key == pygame.K_RIGHT:
+                selected_avatar = (selected_avatar + 1) % len(avatars)
             elif event.key == pygame.K_b:
-                buy_stock()
+                buy_stock(selected_stock)
             elif event.key == pygame.K_s:
-                sell_stock()
+                sell_stock(selected_stock)
+            elif event.key == pygame.K_p:
+                purchase_avatar(selected_avatar)
 
-    update_stock_price()
+    update_stock_prices()
+    selected_stock = pygame.mouse.get_pos()[0] // (WIDTH // len(stocks))
 
     # Clear the screen
     screen.fill(WHITE)
 
-    # Display balance, stock info, and owned stocks
-    balance_text = FONT.render(f"Balance: ${balance:.2f}", True, BLACK)
-    stock_info_text = FONT.render(f"{stock_name}: ${stock_price:.2f}", True, BLACK)
-    stocks_owned_text = FONT.render(f"Stocks owned: {stocks_owned}", True, BLACK)
+    # Display player info
+    balance_text = FONT.render(f"Balance: ${player_balance:.2f}", True, BLACK)
+    avatar_rect = avatars[selected_avatar]
+    screen.blit(avatar_rect, (20, 20))
+    avatar_cost_text = FONT.render(f"Avatar Cost: ${avatar_costs[selected_avatar]}", True, BLACK)
+    stock_info_text = FONT.render(f"Selected Stock: {stocks[selected_stock]['name']} (${stocks[selected_stock]['price']:.2f})", True, BLACK)
+    stocks_owned_text = FONT.render(f"Stocks Owned: {stocks[selected_stock]['quantity']}", True, BLACK)
 
-    screen.blit(balance_text, (20, 20))
-    screen.blit(stock_info_text, (20, 70))
-    screen.blit(stocks_owned_text, (20, 120))
+    screen.blit(balance_text, (20, 250))
+    screen.blit(avatar_cost_text, (20, 300))
+    screen.blit(stock_info_text, (20, 350))
+    screen.blit(stocks_owned_text, (20, 400))
+
+    # Display stock information (same as before)
+    # ...
 
     # Update the display
     pygame.display.flip()
